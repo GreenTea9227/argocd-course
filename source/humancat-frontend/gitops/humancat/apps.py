24b79a649832a4096a175de26c620a561484df4a
GreenTea <sjohn9227@naver.com>
2025-02-26 12:02:45 +0900

    feat: new!!!

--- source/humancat-frontend/gitops/humancat/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HumancatConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'humancat'
--- source/humancat-frontend/gitops/humancat/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
-
-
-def index(request):
-    context = {}
-    return render(request, 'humancat/index.html', context)
-
